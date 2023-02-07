ccc7c1e7c3ae3126ba7571b65b169a1978f830e4
MatheusStefan <roccomatheus0@gmail.com>
2023-02-07 15:02:23 -0300

    Refatoração de código

--- Módulo 1/script.js
+++ Módulo 1/script.js
@@ -1,7 +1,11 @@
-function sumMultiples(n) {
+function start() {
+    var buttonCalculate = document.querySelector('#btn-calc')
+    buttonCalculate.addEventListener('click', handleButtonCalcular)
+}
+function sumMultiples(limit) {
     let sum = 0;
 
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i < limit; i++) {
         if (i % 3 === 0 || i % 5 === 0) {
             sum += i;
         }
@@ -9,12 +13,18 @@ function sumMultiples(n) {
     return sum;
 }
 
-function start() {
-    var input = document.querySelector('#number')
+function handleButtonCalcular() {
+
+    var input = document.querySelector('#input')
+    var result = document.querySelector('#sum-result')
     var A = Number(input.value)
-    var multiplos = sumMultiples(500)
-    input.textContent = multiplos
+
+    var multiplos = sumMultiples(A)
     
+    
+    result.textContent = multiplos
+    console.log(multiplos)
 }
+
 
 start()
